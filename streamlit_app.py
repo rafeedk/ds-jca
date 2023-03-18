060f2b7e29b185f12474c2585cb0765a28052dc5
rafeedk <128207787+rafeedk@users.noreply.github.com>
2023-03-18 12:30:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ fig = px.scatter_mapbox(
     height=500,
     width=800,
     hover_name="Price",
-    hover_data=["Distance from Johan Cruyff Arena", "Location"],
+    hover_data=["Distance from Johan Cruyff Arena", "Location","m"],
     labels={"color": "Locations"},
 )
 fig.update_geos(center=dict(lat=dataframe.iloc[0][2], lon=dataframe.iloc[0][3]))
